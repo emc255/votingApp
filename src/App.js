a9a80a98dc0f573730b80a29a2a3052520e0e6e1
emc <micha3l_255@yahoo.com>
2020-01-24 09:57:08 -0600

    start a project simple voting app in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Title from "./components/Title";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([{ name: "final fantasy", img: "http://placekitten.com/g/200/200", vote: 0 }]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title />
     </div>
   );
 }
---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function Title(props) {
+  return (
+    <div>
+      <h1>TITLE</h1>
+    </div>
+  );
+}
+
+export default Title;
