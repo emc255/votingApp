d181b61ed68dd37f237fffe1c31f7729d05d87c9
emc <micha3l_255@yahoo.com>
2020-01-24 10:34:14 -0600

    add json file

--- src/App.js
+++ src/App.js
@@ -4,16 +4,7 @@ import Body from "./components/Body";
 import "./App.css";
 
 function App() {
-  const [datas, setDatas] = useState([
-    {
-      dataId: 0,
-      userImage: "http://placekitten.com/g/100/100",
-      name: "final fantasy",
-      dateRelease: "Square Enix, 2018",
-      img: "http://placekitten.com/g/200/200",
-      vote: 0
-    }
-  ]);
+  const [datas, setDatas] = useState([]);
 
   const vote = (userVote, dataId) => {};
 
