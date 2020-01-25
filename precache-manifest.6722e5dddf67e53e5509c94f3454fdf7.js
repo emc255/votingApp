1791ea6ab32165dce5e2fec5fe49e0ded3768913
emc <micha3l_255@yahoo.com>
2020-01-25 13:30:42 -0600

    Updates

---
+++ precache-manifest.6722e5dddf67e53e5509c94f3454fdf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f130cf2aef12469afe272ffe67fef139",
+    "url": "/votingApp/index.html"
+  },
+  {
+    "revision": "df23f9c7f26a1fdc54f8",
+    "url": "/votingApp/static/css/main.8a790a76.chunk.css"
+  },
+  {
+    "revision": "56ec29a4c4a893d8f6eb",
+    "url": "/votingApp/static/js/2.920184ed.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/votingApp/static/js/2.920184ed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df23f9c7f26a1fdc54f8",
+    "url": "/votingApp/static/js/main.0c247983.chunk.js"
+  },
+  {
+    "revision": "ce560612ea170f2febb0",
+    "url": "/votingApp/static/js/runtime-main.41917b72.js"
+  }
+]);
